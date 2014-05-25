49e6f531fddf14a81caf791099bb1a1b3049fce1
thedarkjester <code@thenever.co.za>
2014-05-25 13:56:22 +0200

    tests, tests and more tests for value objects

---
+++ src/Aps.BillingCompany/ValueObjects/BillingCompanyUrl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Seterlund.CodeGuard;
+
+namespace Aps.BillingCompanies.ValueObjects
+{
+    public class BillingCompanyUrl
+    {
+        private readonly string url;
+
+        public BillingCompanyUrl(string url)
+        {
+            Guard.That(url).IsNotEmpty();
+            Guard.That(url).IsTrue(s => s.IndexOf("http://", System.StringComparison.InvariantCultureIgnoreCase) == -1, "https is missing");
+
+            Uri testUri;
+            Uri.TryCreate(url, UriKind.Absolute, out testUri);
+
+            if (testUri == null)
+            {
+                throw new ArgumentException();
+            }
+
+            if (url.IndexOf("http", System.StringComparison.InvariantCultureIgnoreCase) == -1)
+            {
+                this.url = "https://" + url;
+            }
+            else
+            {
+                this.url = url;
+            }
+        }
+
+        public BillingCompanyUrl ChangeUrl(string newUrl)
+        {
+            return new BillingCompanyUrl(newUrl);
+        }
+
+        public override string ToString()
+        {
+            return url;
+        }
+    }
+}
---
+++ src/Aps.Core.Tests/BillingCompanyTests/BillingCompanyValueObjectTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Aps.BillingCompanies.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aps.Shared.Tests.BillingCompanyTests
+{
+    [TestClass]
+    public class BillingCompanyValueObjectTests
+    {
+        private string url;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            url = "http://www.site.com";
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void GivenAnEmptyString_WhenConstructingABillingCompanyUrl_ExceptionIsThrown()
+        {
+            //arrange
+            url = "";
+
+            //act
+            var billingCompanyUrl = new BillingCompanyUrl(url);
+
+            //assert
+            //exception attribute
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void GivenAUrlWithHttpAndNotHttps_WhenConstructingABillingCompanyUrl_ExceptionIsThrown()
+        {
+            //arrange
+            url = "http://www.google.com";
+
+            //act
+            var billingCompanyUrl = new BillingCompanyUrl(url);
+
+            //assert
+            //exception attribute
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void GivenUnmakeableUrl_WhenConstructingABillingCompanyUrl_ExceptionIsThrown()
+        {
+            //arrange
+            url = "s.com";
+
+            //act
+            var billingCompanyUrl = new BillingCompanyUrl(url);
+
+            //assert
+            //exception attribute
+        }
+
+        [TestMethod]
+        public void GivenAvalidUrl_WhenConstructingABillingCompanyUrl_NoExceptionIsThrown()
+        {
+            //arrange
+            url = "https://www.google.com";
+
+            //act
+            var billingCompanyUrl = new BillingCompanyUrl(url);
+
+            //assert
+            Assert.IsTrue(billingCompanyUrl.ToString() == "https://www.google.com");
+        }
+    }
+}
