8dac7c4dc6d92644da768fa8f0d0f16088d62de9
thedarkjester <code@thenever.co.za>
2014-05-27 18:28:14 +0200

    more tests

---
+++ src/Aps.Core.Tests/BillingCompanyTests/ScrapingErrorRetryConfigurationTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Aps.BillingCompanies.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aps.Shared.Tests.BillingCompanyTests
+{
+    [TestClass]
+    public class ScrapingErrorRetryConfigurationTests
+    {
+        private int numberOfRetries = 1;
+        private int responseCode = 0;
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Given_LessThanZero_For_ResponseCode_When_Constructing_A_ScrapingErrorRetryConfiguration_ExceptionIsThrown()
+        {
+            //arrange
+            responseCode = -1;
+
+            //act
+            ScrapingErrorRetryConfiguration configuration = new ScrapingErrorRetryConfiguration(responseCode, numberOfRetries);
+
+            //assert
+            //Exception Expected
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Given_LessThanZero_For_NumberOfRetries_When_Constructing_A_ScrapingErrorRetryConfiguration_ExceptionIsThrown()
+        {
+            //arrange
+            numberOfRetries = -1;
+
+            //act
+            ScrapingErrorRetryConfiguration configuration = new ScrapingErrorRetryConfiguration(responseCode, numberOfRetries);
+
+            //assert
+            //Exception Expected
+        }
+
+        [TestMethod]
+        public void Given_AValidValue_For_NumberOfRetries_When_Constructing_A_ScrapingErrorRetryConfiguration_CreationIsSuccessful()
+        {
+            //arrange
+            numberOfRetries = 1;
+            responseCode = 0;
+
+            //act
+            ScrapingErrorRetryConfiguration configuration = new ScrapingErrorRetryConfiguration(responseCode, numberOfRetries);
+
+            //assert
+            Assert.IsTrue(configuration.NumberOfRetries == 1);
+            Assert.IsTrue(configuration.ResponseCode == 0);
+            ;
+        }
+    }
+}
---
+++ src/Aps.Core.Tests/BillingCompanyTests/ScrapingLoadManagementConfigurationTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Aps.BillingCompanies.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aps.Shared.Tests.BillingCompanyTests
+{
+    [TestClass]
+    public class ScrapingLoadManagementConfigurationTests
+    {
+        private int concurrentScrapes = 10;
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void Given_Zero_When_Constructing_A_ScrapingLoadManagementConfiguration_ExceptionIsThrown()
+        {
+            //arrange
+            concurrentScrapes = 0;
+
+            //act
+            ScrapingLoadManagementConfiguration configuration = new ScrapingLoadManagementConfiguration(concurrentScrapes);
+
+            //assert
+            //Exception Expected
+        }
+
+        [TestMethod]
+        public void Given_AValidValue_When_Constructing_A_ScrapingLoadManagementConfiguration_ObjectIsCreatedCorrectly()
+        {
+            //arrange
+            concurrentScrapes = 1;
+
+            //act
+            ScrapingLoadManagementConfiguration configuration = new ScrapingLoadManagementConfiguration(concurrentScrapes);
+
+            //assert
+            Assert.IsTrue(configuration.ConcurrentScrapes == 1);
+        }
+    }
+}
