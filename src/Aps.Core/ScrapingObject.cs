fca24cabcc672a6162e298e21ee587365c64bbda
Wynand <mercury.jaggedice@gmail.com>
2014-05-28 20:53:13 +0200

    Added ScrapingObject with its creator and Repo

---
+++ src/Aps.Core/ScrapingObject.cs
@@ -0,0 +1,39 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aps.Core
+{
+    public class ScrapingObject
+    {
+        public Guid customerId;
+        public Guid billingCompanyId;
+        public readonly string URL;
+        public readonly string plainText1;
+        public readonly string hiddenText1;
+
+        public string scrapeType;
+        public string scrapeStatus;
+        public DateTime timeToRun;
+        public DateTime timeCreated;
+
+        public ScrapingObject(Guid customerId, Guid billingCompanyId, string URL, string plainText1, string hiddenText1)
+        {
+            this.customerId = customerId;
+            this.billingCompanyId = billingCompanyId;
+            this.URL = URL;
+            this.plainText1 = plainText1;
+            this.hiddenText1 = hiddenText1;
+
+            scrapeType = "Register";
+            scrapeStatus = "Active";
+
+            timeToRun = DateTime.Now;
+            timeCreated = DateTime.Now;
+
+         }
+    }
+}
---
+++ src/Aps.Core/ScrapingObjectRepositoryFake.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aps.Core
+{
+    public class ScrapingObjectRepositoryFake
+    {
+        public readonly List<ScrapingObject> scrapingObjects;
+
+        public readonly IEventAggregator eventAggregator;
+        public readonly ScrapingObjectCreator scrapingObjectCreator;
+
+        public ScrapingObjectRepositoryFake(IEventAggregator eventAggregator, ScrapingObjectCreator scrapingObjectCreator)
+        {
+            this.eventAggregator = eventAggregator;
+            this.scrapingObjectCreator = scrapingObjectCreator;
+            this.scrapingObjects = new List<ScrapingObject>();
+        }
+
+        public void StoreScrapingObject(ScrapingObject scrapingObject)
+        {
+            this.scrapingObjects.Add(scrapingObject);
+        }
+
+        public ScrapingObject BuildNewScrapingObject(Guid customerId, Guid billingCompanyId, string URL, string plainText, string hiddenText)
+        {
+            return this.scrapingObjectCreator.GetNewScrapingObject(customerId, billingCompanyId, URL, plainText, hiddenText);
+        }
+
+        public ScrapingObject GetScrapingObjectByCustomerAndBillingCompanyId(Guid customerId, Guid billingCompanyId)
+        {
+            return this.scrapingObjects.FirstOrDefault(x => ((x.customerId == customerId) && (x.billingCompanyId == billingCompanyId)));
+        }
+
+        public IEnumerable<ScrapingObject> GetAllScrapingObjectsByBillingCompanyId()
+        {
+            return scrapingObjects; // currently returns all objects icorreclty - Need to fix it
+        }
+
+    }
+}
