5301bd06f4afcd724efb4bded5f3e4cfd2640ddc
kgangmadtrix <eichlefoko@gmail.com>
2014-06-03 15:26:46 +0200

    Starting work on the translator using TDD

---
+++ src/Aps.Core.Tests/InterpreterTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aps.Core;
+
+namespace Aps.Shared.Tests.InterpreterTests
+{
+    [TestClass]
+    public class ScrapeSessionDataInterpreterTests
+    {
+        [TestMethod]
+        public void WhenReceicingValidXmlScrapeSessionDataPairsShouldBeReturned()
+        {
+            string xml = string.Empty;
+            Interpreter interpreter = new Interpreter();
+            var r = interpreter.TransformResults(xml);
+        }
+    }
+
+
+}
---
+++ src/Aps.Core/Interpreters/Interpreter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aps.Core
+{
+    public class Interpreter
+    {
+        public List<DataPair> TransformResults(string xml)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
