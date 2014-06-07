252ae611e056f039e3d22f4836ad83d0b2b00f63
kgangmadtrix <eichlefoko@gmail.com>
2014-06-07 09:26:28 +0200

    Removed validation result classes

--- src/Aps.Core.Tests/ValidatorTests/ScrapeSessionDataValidatorTests.cs
+++ src/Aps.Core.Tests/ValidatorTests/ScrapeSessionDataValidatorTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Aps.Core.Validation;
 using Aps.Core;
+using Aps.Core.Validation;
 
 namespace Aps.Shared.Tests.ValidatorTests
 {
@@ -12,20 +12,7 @@ namespace Aps.Shared.Tests.ValidatorTests
         public void WhenReceicingScrapeSessionDataValidationResultsShouldBeReturned()
         {
             ScrapeSessionDataValidator scrapeSessionDataValidator = new ScrapeSessionDataValidator();
-            ScrapeSessionData scrapeSessionData = new ScrapeSessionData();
-            ValidationResults vResults = scrapeSessionDataValidator.validateScrapeData(scrapeSessionData);
-            if (vResults.IsValid)
-            {
-                //successful validation with no validation errors
-            }
-            else
-            {
-                foreach (var vResult in vResults.Results)
-                {
-                    //check which datapairs validated insuccessfully and their error codes.
-                    
-                }
-            }
+
         }
     }
 }
---
+++ src/Aps.Core/Interpreters/ScrapeNoDataPairsFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aps.Core.Interpreters
+{
+    public class ScrapeNoDataPairsFoundException : Exception
+    {
+        public ScrapeNoDataPairsFoundException()
+        {
+
+        }
+
+        public ScrapeNoDataPairsFoundException(string message)
+        : base(message)
+        {
+
+        }
+
+        public ScrapeNoDataPairsFoundException(string message, Exception inner)
+        : base(message, inner)
+        {
+
+        }
+
+        public ErrorCode ErrorCode { get; set; }
+    }
+}
