60e5caa7a5f855da0457dc7ddde9e8c9d69c99d4
jigneshnarain <jignesh.narain@gmail.com>
2014-06-07 11:21:29 +0200

    Add a fake webscraper, add new aps.scraping.test, add scrapesessioninitiator and scraperOrchestrators, also the Moq framework for test

---
+++ src/Aps.Scraping.Test/WebScraperTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Aps.Scraping.WebScrapers;
+
+namespace Aps.Scraping.Test
+{
+    [TestClass]
+    public class WebScraperTest
+    {
+        Mock<WebScraperFake> mockWebScraper;
+        string url;
+        string username;
+        string password;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockWebScraper = new Mock<WebScraperFake>();
+
+            url ="http://www.telkom.co.za";
+            username = "dave";
+            password = "d@v3!";
+        }
+
+        [TestMethod]
+        public void Given_UrlAndUserCredentials_Then_PerformScrape()
+        {
+            mockWebScraper.Object.Scrape(url, username, password);
+
+            mockWebScraper.Verify();
+
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void Given_Null_Parameter_For_Url_WhenInitiatingAScrape_Exception_IsThrown()
+        {
+            url = null;
+            mockWebScraper.Object.Scrape(url, username, password);
+           
+        }
+
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void Given_Null_Parameter_For_Username_WhenInitiatingAScrape_Exception_IsThrown()
+        {
+            username = null;
+            mockWebScraper.Object.Scrape(url, username, password);
+
+   
+        }
+
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void Given_Null_Parameter_For_Password_WhenInitiatingAScrape_Exception_IsThrown()
+        {
+            password = null;
+            mockWebScraper.Object.Scrape(url, username, password);
+
+          
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void Given_Invalid_Parameter_For_Url_WhenInitiatingAScrape_Exception_IsThrown()
+        {
+            url = "Hello";
+            mockWebScraper.Object.Scrape(url, username, password);
+
+           
+        }
+    }
+}
---
+++ src/Aps.Scraping/WebScrapers/WebScraperFake.cs
@@ -0,0 +1,20 @@
+﻿using Seterlund.CodeGuard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aps.Scraping.WebScrapers
+{
+    public class WebScraperFake : IWebScraper
+    {
+        public string Scrape(string url, string username, string password)
+        {
+            Guard.That(url).IsNotNullOrEmpty().IsTrue(x => Uri.IsWellFormedUriString(url, UriKind.Absolute), "Invalid url");
+            Guard.That(username).IsNotNullOrEmpty();
+            Guard.That(password).IsNotNullOrEmpty();
+            return @"<scrape-session><base-url>www.telkom.co.za</base-url><date>10/01/2008</date><time>13:50:00</time><datapair id=001><text>Account no</text><value>53844946068883</value></datapair><datapair id=002><text>Service ref</text><value>0117838898</value></datapair><datapair id=003><text>Previous Invoice</text><value>R512.22</value></datapair><datapair id=004><text>Payment</text><value>R513.00</value></datapair><datapair id=005><text>Opening Balance</text><value>R0.78</value></datapair></scrape-session>";
+        }
+    }
+}
