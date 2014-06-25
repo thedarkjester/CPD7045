654eb1549e9bd4215c7f97f9189882bc66216f5a
thedarkjester <code@thenever.co.za>
2014-06-25 20:12:23 +0200

    image and document updates

--- src/Aps.BillingCompany/ValueObjects/BillingCompanyName.cs
+++ src/Aps.BillingCompany/ValueObjects/BillingCompanyName.cs
@@ -32,5 +32,50 @@ namespace Aps.BillingCompanies.ValueObjects
         {
             return name;
         }
-    } 
+
+        public override bool Equals(System.Object obj)
+        {
+            BillingCompanyName companyName = obj as BillingCompanyName;
+            if ((object)companyName == null)
+            {
+                return false;
+            }
+
+            return  Name == companyName.Name;
+        }
+
+        public bool Equals(BillingCompanyName companyName)
+        {
+            // Return true if the fields match:
+            return Name == companyName.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public static bool operator ==(BillingCompanyName a, BillingCompanyName b)
+        {
+            // If both are null, or both are same instance, return true.
+            if (System.Object.ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            // If one is null, but not both, return false.
+            if (((object)a == null) || ((object)b == null))
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return a.Name == b.Name;
+        }
+
+        public static bool operator !=(BillingCompanyName a, BillingCompanyName b)
+        {
+            return !(a == b);
+        }
+    }
 }
